772b290a3904651916fd188ee9b9ef3746d9a8c6
haotian <18296517259@163.com>
2020-04-04 17:25:14 +0800

    Initial commit

---
+++ src/main/java/com/five/auok/AuokApplication.java
@@ -0,0 +1,13 @@
+package com.five.auok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuokApplication.class, args);
+    }
+
+}
