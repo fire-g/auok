078f073d13752acf84ba5c06d44ee4279a252ede
Forever9-love <58922546+Forever9-love@users.noreply.github.com>
2020-05-06 12:30:56 +0800

    bug修改

--- src/main/java/edu/auok/repository/DoctorsRepo.java
+++ src/main/java/edu/auok/repository/DoctorsRepo.java
@@ -4,12 +4,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import edu.auok.model.Doctor;
 
-public interface DoctorsRepo extends JpaRepository<Doctor, Integer>{
+@Repository
+public interface DoctorsRepo extends JpaRepository<Doctor,Integer>{
 	
-	@Query("select d from doctors d where name like ?1")
+	@Query("select d from doctors d where d.name like ?1")
 	public Page<Doctor> findByKeyword(String kw, Pageable pageable);
 	
 }
