16339fb8a48711c3b2c672b52652e821c83b9147
Forever9-love <58922546+Forever9-love@users.noreply.github.com>
2020-05-07 15:45:30 +0800

    1

--- src/main/java/edu/auok/web/DoctorsController.java
+++ src/main/java/edu/auok/web/DoctorsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DoctorsController {
 	
 	@RequestMapping("/listDoctors")
-	public String welcome() {
+	public String listDoctors() {
 		return "listDoctors";
 	}
 	
