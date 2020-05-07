b6469367b283413b194f31a601245d84a3f04a59
Forever9-love <58922546+Forever9-love@users.noreply.github.com>
2020-05-07 11:29:11 +0800

    1

--- src/main/java/edu/auok/rest/DoctorsRestController.java
+++ src/main/java/edu/auok/rest/DoctorsRestController.java
@@ -5,8 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.auok.model.Doctor;
 import edu.auok.service.DoctorsService;
@@ -18,8 +18,8 @@ public class DoctorsRestController {
 	@Autowired
 	private DoctorsService doctorsService;
 	
-	@RequestMapping("/")
-	public String list(String kw, Model model, Pageable pageable) {
+	@GetMapping("")
+	public String listDocotrs(String kw, Model model, Pageable pageable) {
 		if(kw != null) {
 			kw = "%" + kw + "%";
 		}else {
--- src/main/java/edu/auok/web/AppointmentController.java
+++ src/main/java/edu/auok/web/AppointmentController.java
@@ -6,9 +6,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppointmentController {
 	
-	@RequestMapping("/listDoctors")
-	public String welcome() {
-		return "listDoctors";
-	}
 	
 }
