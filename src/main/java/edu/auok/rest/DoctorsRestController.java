2b4a6c0fe505f770c6ca70ff149fbbc05a421081
LUZaLID-lzl <LUZaLID@outlook.com>
2020-05-07 12:46:24 +0800

    1

--- src/main/java/edu/auok/rest/DoctorsRestController.java
+++ src/main/java/edu/auok/rest/DoctorsRestController.java
@@ -3,11 +3,16 @@ package edu.auok.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.appointment.model.doctor;
 import edu.auok.model.Doctor;
 import edu.auok.service.DoctorsService;
 
@@ -32,4 +37,9 @@ public class DoctorsRestController {
 		return "listdoctors";
 	}
 	
+	@RequestMapping(value="/maxpatient",method = RequestMethod.POST,produces="application/json")
+	public ResponseEntity<doctor> setMaxpatient(@RequestBody int id,int maxpatient){
+		clinicService.setMaxpatient(id, maxpatient);
+		return new ResponseEntity<doctor>(HttpStatus.CREATED);
+	}
 }
